8c96a51eb4fa4a1afdac56824de7b585ab17083b
Salando <saulorlandozzz@gmail.com>
2026-08-19 18:14:08 -0600

    :card_file_box: feat: se conecto a la base de datos y se hizo un Homecontroller basico

---
+++ src/main/java/org/esfe/controladores/HomeController.java
@@ -0,0 +1,13 @@
+package org.esfe.controladores;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+}
