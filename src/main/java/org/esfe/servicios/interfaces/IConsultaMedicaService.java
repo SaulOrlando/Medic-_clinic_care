45aff43ae4c19455cd37529195e0c512ad885ea2
Salando <saulorlandozzz@gmail.com>
2026-08-21 19:22:48 -0600

    [Scrum-6] Saul Tobar: Se creo la interfaz y el servicio de consulta medica

---
+++ src/main/java/org/esfe/servicios/interfaces/IConsultaMedicaService.java
@@ -0,0 +1,25 @@
+package org.esfe.servicios.interfaces;
+
+import org.esfe.modelos.ConsultaMedica;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IConsultaMedicaService {
+
+    List<ConsultaMedica> obtenerTodos();
+
+    Optional<ConsultaMedica> obtenerPorId(Integer idConsulta);
+
+    Optional<ConsultaMedica> buscarPorCita(Integer idCita);
+
+    List<ConsultaMedica> buscarPorMedico(Integer idMedico);
+
+    List<ConsultaMedica> buscarPorPaciente(Integer idPaciente);
+
+    ConsultaMedica registrarConsulta(ConsultaMedica consulta);
+
+    ConsultaMedica guardar(ConsultaMedica consulta);
+
+    void eliminar(Integer idConsulta);
+}
